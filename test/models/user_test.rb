3975014e7703fe2dfd5b81289d178b0c51cde175
Magnolia <magnolia1925pa@gmail.com>
2020-04-14 11:09:18 +0900

    add presence test in users model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Len Kiryu", email: "user@example.com")
+  end
+
+  test "expect valid" do
+    assert @user.valid?
+  end
+
+  test "expect name present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "expect email present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
 end

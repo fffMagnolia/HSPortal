fac852eac1c53c480034ae6d216d57e6d448ebf0
Magnolia <magnolia1925pa@gmail.com>
2020-04-16 15:53:51 +0900

    add before action users for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
-  # validationはemail_validatorで行っている
   validates :email, presence: true, length: { maximum: 255 }, email: true, uniqueness: true
+
+  before_save { self.email = self.email.downcase.strip }
 end

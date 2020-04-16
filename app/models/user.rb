c697b99c5b7e8abbbfe02d06db2da84d6f3d1e26
Magnolia <magnolia1925pa@gmail.com>
2020-04-16 15:46:28 +0900

    add validation users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  # validationはemail_validatorで行っている
+  validates :email, presence: true, length: { maximum: 255 }, email: true, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -18,4 +18,37 @@ class UserTest < ActiveSupport::TestCase
     @user.email = ""
     assert_not @user.valid?
   end
+
+  test "expect name max-length 50 char" do
+    @user.name = 'a' * 51
+    assert_not @user.valid?
+  end
+
+  test "expect email max-length 255 char" do
+    @user.email = "a"*244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "expect email format valid" do
+    valid_addr = [ 'user123@456example.com', 'USER@EXAMPLE.com', 'user+_-@example.com', 'user@example-foo.bar.com' ]
+    valid_addr.each do |addr|
+      @user.email = addr
+      assert @user.valid?, "#{addr} should be valid!"
+    end
+  end
+
+  test "expect email format invalid" do
+    invalid_addr = [ 'user @email.com', 'user@ email.com' ]
+    invalid_addr.each do |addr|
+      @user.email = addr
+      assert_not @user.valid?, "#{addr} should be invalid!"
+    end
+  end
+
+  test "expect email is unique and BIG CHAR the same normal char" do
+    dup_user = @user.dup
+    @user.save
+    dup_user.email.upcase
+    assert_not dup_user.valid?
+  end
 end

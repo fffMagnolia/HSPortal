d1d7360a3fbd9033b1c88a1231799a7efea74037
Magnolia <magnolia1925pa@gmail.com>
2020-04-22 12:03:12 +0900

    add helper for application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(add_title = '')
+    base_title = 'HSPortal'
+    if add_title.empty?
+      base_title
+    else
+      add_title + " | " + base_title
+    end
+  end
 end

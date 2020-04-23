0766a9e8239a1a43fb04bf0eb17abcb8fb0e4fec
Magnolia <magnolia1925pa@gmail.com>
2020-04-23 22:00:32 +0900

    add integration_test for signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "should fail signup" do
+    get new_user_path
+    assert_no_difference "User.count" do
+      post users_path, params: {
+        user: {
+          name: "",
+          email: "invalid@com",
+          password: "",
+          password_confirmation: ""
+        } }
+    end
+    assert_template 'users/new'
+  end
+end

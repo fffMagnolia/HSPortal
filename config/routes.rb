2a0f04b3c2d226441483c648237594dbbdbcf459
Magnolia <magnolia1925pa@gmail.com>
2020-04-24 22:29:12 +0900

    add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'

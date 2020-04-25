b4504a86c23e266918209e70af2b07002f65180e
Magnolia <magnolia1925pa@gmail.com>
2020-04-25 17:39:26 +0900

    add new action sessions_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get '/policy', to: 'static_pages#policy'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 end

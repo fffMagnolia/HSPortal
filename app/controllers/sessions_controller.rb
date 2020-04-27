dbbd8ba80cc8bd8e97285ce902cc22fd22a4f648
Magnolia <magnolia1925pa@gmail.com>
2020-04-27 22:51:04 +0900

    add action create sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+
+  # access
   def new
+  end
+
+  # login
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      # when success 
+    else
+      flash.now[:danger] = "Email or Password Invalid."
+      render 'new'
+    end
+  end
+
+  # logout
+  def destroy
   end
 end

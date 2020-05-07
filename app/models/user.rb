111fd11b1455c6edd2bd6117b225fe19d50b1936
Magnolia <magnolia1925pa@gmail.com>
2020-05-07 21:40:05 +0900

    update users_login_test and for the system

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,15 @@ class User < ApplicationRecord
   validates :email,    presence: true, length: { maximum: 255 }, email: true, uniqueness: { case_sensitive: false }
   validates :password,                 length: { minimum: 8 }
 
+  has_secure_password
+
   def email=(e)
     e = e.gsub(" ", "").downcase if e
     super
   end
 
-  has_secure_password
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine::cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:alice)
+  end
+
   test "except login error flash only login view" do
     get login_path
     assert_template 'sessions/new'
@@ -14,5 +19,20 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     # フラッシュが消えていることを期待
     assert flash.empty?
+  end
+
+  test "except login" do
+    get login_path
+    post login_path, params: { session: {
+      email: @user.email,
+      password: "password"
+    } }
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", login_path, count: 0 
+    # 切り替え表示後に表示されていることを期待
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end

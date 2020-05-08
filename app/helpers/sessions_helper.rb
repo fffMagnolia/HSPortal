f71c2f883ea41dcc20ce035bfbe317bb2afaca0f
Magnolia <magnolia1925pa@gmail.com>
2020-05-08 10:49:23 +0900

    add remember me system

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,9 +3,16 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+  # 利便性のためuser_idに値を入れている
   def current_user
-    if session[:user_id]
-      @current_user = User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
@@ -14,7 +21,22 @@ module SessionsHelper
   end
 
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # セッションを永続化
+  def remember
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  # 永続セッションの情報を破棄
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 end

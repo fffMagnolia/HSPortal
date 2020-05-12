ce3b1e514d05ad4c91b92b023c8376c83e104348
Magnolia <magnolia1925pa@gmail.com>
2020-05-12 22:55:07 +0900

    update seed and fixture

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  name: "Alice in Wonderland",
+  email: "alice@example.com",
+  password: 'foobarbaz',
+  password_confirmation: 'foobarbaz',
+  activated: true,
+  activated_at: Time.zone.now
+)

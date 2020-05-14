f0e82ee7ff0f1520589c87dc16c186d2cd3f9c37
Magnolia <magnolia1925pa@gmail.com>
2020-05-14 23:31:24 +0900

    update authenticated? method for activation system

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -72,6 +72,6 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "expect false when remember_digest was nil" do
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
   end
 end

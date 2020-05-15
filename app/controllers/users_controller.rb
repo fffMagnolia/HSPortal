c9ef0d558fa1f5c0a1b526b2d2e008783e458018
Magnolia <magnolia1925pa@gmail.com>
2020-05-15 14:03:03 +0900

    refactor a little code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = '登録されたメールアドレスにメールを送信しました。ご確認ください。 '
       redirect_to root_url
     else

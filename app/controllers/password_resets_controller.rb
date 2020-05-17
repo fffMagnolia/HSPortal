1454078b6d39090f6472dfd2fba1508e7993af51
Magnolia <magnolia1925pa@gmail.com>
2020-05-17 21:06:15 +0900

    update mailer and password_reset C&M

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,21 @@
 class PasswordResetsController < ApplicationController
+  # NOTE: new/edit actionはview表示のためだけに必要
+
   def new
+  end
+
+  def  create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.sent_password_reset_email
+      flash[:info] = "パスワード再設定用のメールを送信しました。ご確認ください。"
+      # ログインフォームの方が使い勝手がいいかもしれない
+      redirect_to root_url
+    else
+      flash[:danger] = "メールアドレスが登録されていません。"
+      render 'new'
+    end
   end
 
   def edit

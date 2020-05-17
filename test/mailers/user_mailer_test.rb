6e615f3bb9aac07529795b53a4ed29470d236338
Magnolia <magnolia1925pa@gmail.com>
2020-05-17 21:10:52 +0900

    update mailer test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -15,13 +15,15 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match CGI.escape(user.email), mail.text_part.body.decoded.encode('UTF-8')
   end
 
-=begin
   test "password_reset" do
-    mail = UserMailer.password_reset
-    assert_equal "Password reset", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    user = users(:alice)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal "パスワードの再設定", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.name, mail.text_part.body.decoded.encode('UTF-8')
+    assert_match user.reset_token, mail.text_part.body.decoded.encode('UTF-8')
+    assert_match CGI.escape(user.email), mail.text_part.body.decoded.encode('UTF-8')
   end
-=end
 end

65200724bfc047b53ef0596c1cd7bacbca47fb27
Magnolia <magnolia1925pa@gmail.com>
2020-05-17 20:42:16 +0900

    add controller password_resets

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
@@ -12,4 +14,5 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end

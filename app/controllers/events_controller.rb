9585d92c4d7bebd49faa7273872a280d5c427d80
Magnolia <magnolia1925pa@gmail.com>
2020-05-21 17:35:59 +0900

    add action in event and test

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,20 +1,41 @@
 class EventsController < ApplicationController
 
-  before_action :logged_in_user, only: [:create, :edit]
+  before_action :logged_in_user, only: [:new, :create, :edit, :update]
 
   def show
     @event = Event.find(params[:id])
   end
 
+  def new
+    @event = Event.new
+  end
+
   def create
+    @event = Event.new(event_params)
+    if @event.save
+      flash[:success] = "イベント[#{@event.title}]を作成しました。"
+      redirect_to events_url
+    else
+      render 'new'
+    end
   end
 
   def edit
     @event = Event.find(params[:id])
   end
 
+  def update
+  end
+
   def index
     @events = Event.all
   end
+
+  private
+
+    # user_idを弄れないようにしている
+    def event_params
+      params.require(:event).permit(:title, :content, :start_date, :end_date)
+    end
 
 end

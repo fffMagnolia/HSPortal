c6186ab7b4a5b7867cb9577e271aad8ea40f4549
Magnolia <magnolia1925pa@gmail.com>
2020-05-22 00:05:46 +0900

    add message column users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_092137) do
+ActiveRecord::Schema.define(version: 2020_05_21_150409) do
 
   create_table "events", force: :cascade do |t|
     t.text "content"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_05_20_092137) do
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_send_at"
+    t.text "message"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

076cae7cf05927b6b1bf18645806a1072e5c3382
Magnolia <magnolia1925pa@gmail.com>
2020-05-28 18:50:07 +0900

    fix entry logic

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,15 @@ module EventsHelper
   def set_default_capacity(event)
     event.capacity = 30
   end
+
+  # 定員に到達していないことを期待
+  def capacity_over?(event)
+    event.entries.count < event.capacity
+  end
+
+  # 開始時刻1時間前以前であることを期待
+  def time_over?(event)
+    entry_limit = event.start_date - 1.hours
+    entry_limit > Time.zone.now
+  end
 end

1b346183a8073f8231c9d95d2a1e7dcd229a30af
Magnolia <magnolia1925pa@gmail.com>
2020-05-28 12:32:48 +0900

    add column info in users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_094438) do
+ActiveRecord::Schema.define(version: 2020_05_28_033130) do
 
   create_table "entries", force: :cascade do |t|
     t.integer "user_id"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_05_27_094438) do
     t.string "reset_digest"
     t.datetime "reset_send_at"
     t.text "message"
+    t.string "info"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

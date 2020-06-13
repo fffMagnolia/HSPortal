ce974038137443aab6604b2904f84f8b8334ea13
Magnolia <magnolia1925pa@gmail.com>
2020-06-13 17:13:05 +0900

    add route inquiries

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  # === root ===
   root 'static_pages#home'
+  # === static pages ===
   get '/explain', to: 'static_pages#explain'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get '/policy', to: 'static_pages#policy'
+  # === rename ===
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
@@ -17,4 +20,5 @@ Rails.application.routes.draw do
   resources :events, only: [:new, :create, :edit, :update, :index, :show]
   resources :entries, only: [:create]
   resources :infos, only: [:new, :create, :show, :destroy]
+  resources :inquiries, only: [:new, :create]
 end

4d2639e130613ae0a44ecfb3ff9004e56c148e1e
Magnolia <magnolia1925pa@gmail.com>
2020-06-13 15:22:05 +0900

    fix bug

--- test/integration/events_edit_test.rb
+++ test/integration/events_edit_test.rb
@@ -20,16 +20,18 @@ class EventsEditTest < ActionDispatch::IntegrationTest
   test "expect success update when content and title nil" do
     log_in(@user)
     event = @user.events.first
-    update_date = Time.zone.now.strftime("%Y-%m-%d %H:%M")
+    #update_date = Time.zone.now.strftime("%Y-%m-%d %H:%M")
+    update_date = Time.zone.now
     patch event_path(event), params: { event: {
       content: event.content,
       start_date: 1.hours.ago,
       end_date: update_date,
-      title: event.title 
+      title: event.title
     } }
     assert_not flash.empty?
     assert_redirected_to events_url
     event.reload
-    assert_equal event.end_date, update_date
+    #assert_equal event.end_date, update_date
+    assert_in_delta event.end_date, update_date, 1.second
   end
 end
